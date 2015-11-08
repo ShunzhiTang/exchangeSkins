6e8e97c017b60942a2e171b9b0948583b65eb32f
tangShunZhi <1046975221@qq.com>
2015-11-07 19:25:49 -0800

    1、点击选项卡的时间把title存储 归档 2、在根据点击的不同取出不同 选项卡的名称根据文件的不同实现 点击选项卡 切换颜色

--- exchangeSkins/exchangeSkins/ViewController.m
+++ exchangeSkins/exchangeSkins/ViewController.m
@@ -13,6 +13,7 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView *rect;
 
+@property (weak ,nonatomic) IBOutlet UISegmentedControl *segmented;
 @end
 ///Users/tang/Desktop/iOS代码/知识扩展/同名图片换肤问题/exchangeSkins/exchangeSkins/exchangeSkins/skin/blue/face.png
 @implementation ViewController
@@ -25,8 +26,15 @@
 }
 
 
+/**
+ *  皮肤设置界面 提供选项卡 决定皮肤样式
+ *
+ *  @param senderv
+ */
+
 - (IBAction)changeSkin:(UISegmentedControl *)sender {
     
+    /*
     // 第二种方法是 使用 bundle 去 加载 同名的 image
     switch (sender.selectedSegmentIndex) {
         case 3://绿色
@@ -44,15 +52,38 @@
             break;
         default:
             break;
-    }
+    }*/
+
     
+    // titleForSegmentAtIndex 获取当前选项的标题
+    [[NSUserDefaults standardUserDefaults] setObject:[sender titleForSegmentAtIndex:sender.selectedSegmentIndex] forKey:@"skin"];
+    
+    [self setSkin];
 }
+
+
+/**
+ *  根据归档的valu去设置控件
+ */
+- (void)setSkin{
+    
+    //解档 得到 存储的标题
+    NSString *titleColor = [[NSUserDefaults standardUserDefaults] valueForKey:@"skin"];
+    
+//    NSLog(@"%@",titleColor);
+    self.face.image = [UIImage imageNamed:[NSString stringWithFormat:@"skin/%@/face.png" , titleColor]];
+    self.heart.image = [UIImage imageNamed:[NSString stringWithFormat:@"skin/%@/heart.png" , titleColor]];
+    self.rect.image = [UIImage imageNamed:[NSString stringWithFormat:@"skin/%@/rect.png" , titleColor]];
+}
+
+
+
 
 //- (void)viewDidAppear:(BOOL)animated{
 //    [super viewDidAppear:animated];
 //
 
-    //第一中方法 ，直接拖进来实体的文件  ， 直接 获取 图片名
+    //第一种方法 ，直接拖进来实体的文件  ， 直接 获取 图片名
 //     self.face.image = [UIImage imageNamed:@"skin/blue/face.png"];
 //}
 
